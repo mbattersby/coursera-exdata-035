8510e7913a124c1d974f0d9e469b194a3af567aa
Mike "Xodiv" Battersby <mib@post.com>
2015-12-22 17:48:19 +1100

    Bar or scatter verions for plot1 and plot2

--- Project2/plot1.R
+++ Project2/plot1.R
@@ -4,24 +4,61 @@
 
 source('fetch.dataset.R')
 
-plot1 <- function () {
-
+plot1bar <- function () {
+    
     total_pm25 <- pm25 %>% group_by(year) %>% summarize(total_emissions = sum(Emissions) / 1e6)
-
+    
     par(mar=c(3,6,3,1))
     
-    p <-barplot(total_pm25$total_emissions,
-                names.arg=total_pm25$year,
-                col="wheat",
-                ylim=c(0,8),
-                ylab='Total PM25 Emissions\n(x 1e6 tons)')
-    
-    text(p, total_pm25$total_emissions,
-         label = paste(round(total_pm25$total_emissions, digits = 2), 'M', sep=''),
-         pos = 3,
-         col = "steelblue")
-    
-    title('Total PM2.5 Emissions By Year')
+    with(total_pm25, {
+        x_offsets <-barplot(total_emissions,
+                            names=year,
+                            col="wheat",
+                            ylim=c(0,8),
+                            ylab='Total PM2.5 Emissions\n(x 1e6 tons)')
+        
+        text(x_offsets, total_emissions,
+             label = paste(round(total_emissions, digits = 2), 'M', sep=''),
+             pos = 3,
+             col = "steelblue")
+        
+        points(x_offsets, total_emissions, pch=15)
+        lines(x_offsets, total_emissions, lty=5)
+        
+        title('Total PM2.5 Emissions By Year')
+    })
 }
 
-plot1()
+plot1scatter <- function () {
+    
+    total_pm25 <- pm25 %>%
+        group_by(year) %>%
+        summarize(total_emissions = sum(Emissions) / 1e6)
+    
+    par(mar=c(3,6,3,1))
+    
+    with(total_pm25, {
+        
+        # The return value from barplot is the x positions of the middle of each bar
+        
+        plot(year, total_emissions, pch=19,
+             xlim=c(1998,2009),
+             ylim=c(0,9),
+             ylab='Total PM2.5 Emissions\n(x 1000 tons)')
+        
+        lines(year, total_emissions)
+        
+        abline(lm(total_emissions ~ year), lty=5, col='red')
+        
+        # So we can use the X positions to place our text values
+        
+        text(year, total_emissions,
+             label = paste(round(total_emissions, digits = 2), 'M', sep=''),
+             pos = 3,
+             col = "steelblue")
+        
+        title('Baltimore PM2.5 Emissions By Year')
+    })
+}
+
+plot1scatter()
--- Project2/plot2.R
+++ Project2/plot2.R
@@ -1,3 +1,74 @@
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland
 # (fips == "24510") from 1999 to 2008? Use the base plotting system to make a
 # plot answering this question.
+
+source('fetch.dataset.R')
+
+plot2bar <- function () {
+    
+    balt_pm25 <- pm25 %>%
+        filter(fips == 24510) %>%
+        group_by(year) %>%
+        summarize(total_emissions = sum(Emissions) / 1e3)
+    
+    par(mar=c(3,6,3,1))
+    
+    with(balt_pm25, {
+        
+        # The return value from barplot is the x positions of the middle of each bar
+        
+        x_offsets <- barplot(total_emissions,
+                             names=year,
+                             col="wheat",
+                             ylim=c(0,4),
+                             ylab='Total PM2.5 Emissions\n(x 1000 tons)')
+        
+        
+        # So we can use the X positions to place our text values
+        
+        text(x_offsets, total_emissions,
+             label = paste(round(total_emissions, digits = 2), 'M', sep=''),
+             pos = 3,
+             col = "steelblue")
+        
+        points(x_offsets, total_emissions, pch=15)
+        lines(x_offsets, total_emissions, lty=5)
+        
+        title('Baltimore PM2.5 Emissions By Year')
+    })
+}
+
+plot2scatter <- function () {
+    
+    balt_pm25 <- pm25 %>%
+        filter(fips == 24510) %>%
+        group_by(year) %>%
+        summarize(total_emissions = sum(Emissions) / 1e3)
+    
+    par(mar=c(3,6,3,1))
+    
+    with(balt_pm25, {
+        
+        # The return value from barplot is the x positions of the middle of each bar
+        
+        plot(year, total_emissions, pch=19,
+             xlim=c(1998,2009),
+             ylim=c(0,4),
+             ylab='Total PM2.5 Emissions\n(x 1000 tons)')
+        
+        lines(year, total_emissions)
+        
+        abline(lm(total_emissions ~ year), lty=5, col='red')
+        
+        # So we can use the X positions to place our text values
+        
+        text(year, total_emissions,
+             label = paste(round(total_emissions, digits = 2), 'M', sep=''),
+             pos = 3,
+             col = "steelblue")
+        
+        title('Baltimore PM2.5 Emissions By Year')
+    })
+}
+
+plot2scatter()
