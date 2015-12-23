12ab849ed5763dc88b9c8258a256c3e4970498fd
Mike "Xodiv" Battersby <mib@post.com>
2015-12-24 09:49:42 +1100

    Start of plot6.

--- Project2/plot6.R
+++ Project2/plot6.R
@@ -2,3 +2,29 @@
 # emissions from motor vehicle sources in Los Angeles County, California
 # (fips == "06037"). Which city has seen greater changes over time in motor
 # vehicle emissions?
+
+source('fetch.dataset.R')
+
+library(ggplot2)
+suppressPackageStartupMessages(library(dplyr))
+
+plot6 <- function () {
+    
+    balt_pm25 <- pm25 %>%
+        filter(fips == '24510' | fips == '06037') %>%
+        filter(grepl('^Mobile - On-Road', EI.Sector)) %>%
+        group_by(year, fips) %>%
+        summarize(total_emissions = sum(Emissions))
+    
+    p <- qplot(year, total_emissions, data=balt_pm25) +
+        facet_wrap(~fips, ncol=2, scale='free') +
+        geom_line() +
+        geom_smooth(method="lm", se=TRUE, linetype=8, col='red') +
+        labs(y = 'Total PM2.5 Emissions (tons)') +
+        labs(x = 'Year') +
+        geom_hline(yintercept=0) +
+        labs(title = 'Motor Vehicle PM2.5 Emissions By Year')
+    print(p)
+}
+
+plot6()
